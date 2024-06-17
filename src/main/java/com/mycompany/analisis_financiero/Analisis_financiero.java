9801764268432d60e7684ab5f016cf030cd08dde
JosueRodriguezYanes <166240738+JosueRodriguezYanes@users.noreply.github.com>
2024-06-17 10:36:26 -0600

    Add files via upload

---
+++ src/main/java/com/mycompany/analisis_financiero/Analisis_financiero.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.analisis_financiero;
+
+/**
+ *
+ * @author User
+ */
+public class Analisis_financiero {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
